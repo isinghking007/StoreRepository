0dd8af668a1a30ea5c0190dddc9b7b215459b2f0
isinghking007 <007ravinash@gmail.com>
2025-03-20 09:17:40 +0530

    CSS Issue fixed

--- UI/StoreUI/src/app/Components/Home/home/home.component.ts
+++ UI/StoreUI/src/app/Components/Home/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import {MatGridListModule} from '@angular/material/grid-list';
 import { Router } from '@angular/router';
 import { CommonservicesService } from '../../../Services/commonservices.service';
@@ -29,11 +29,20 @@ export class HomeComponent implements OnInit {
   isAuthenticated:boolean=false;
   isMenuOpen = false;
   isMenuClicked=true;
+  screenWidth = window.innerWidth;
   loggedInUserEmail:string="";
   loggedInUserName:string="";
+window: any;
+isLargeScreen = window.innerWidth >= 768; // Initial check
   constructor(private route:Router,private authService:CommonservicesService){
     this.isAuthenticated = this.authService.isAuthenticated();
-    
+    window.addEventListener('resize', () => {
+      this.screenWidth = window.innerWidth;
+      if (this.screenWidth < 768) {
+        this.isMenuClicked = false; // Ensure menu stays hidden on small screens
+      }
+    });
+  
   }
  
 
@@ -48,12 +57,27 @@ export class HomeComponent implements OnInit {
 );
     this.loggedInUserEmail=decodeDetails.email||"Guest";
   }
-
+  @HostListener('window:resize', [])
+  onResize() {
+    this.isLargeScreen = window.innerWidth >= 768;
+  }
   logout() {
     this.authService.logout();
     this.route.navigate(['/login']);
   }
   toggleAuth(){ 
     this.isMenuClicked=!this.isMenuClicked;
+}
+
+
+toggleMenu() {
+  console.log(this.screenWidth);
+  if (this.screenWidth >= 768) {
+    console.log("inside toggle menu");
+    
+    this.isMenuClicked = !this.isMenuClicked;
+    console.log(this.isMenuOpen);
+  }
+ 
 }
 }
